8b9fbdeda5b445b16eeba2d8fcb60a4553dddef1
dab1ca <tsvetantsvetanov89@gmail.com>
2023-03-27 20:40:10 +0300

    dbc

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -10,4 +10,4 @@ USE cv;
 
 CREATE TABLE jobs (id int primary key auto_increment, job_title varchar(50), employer_name varchar(50), period_time varchar(50), job_description text(5000), employment_start_date date(), employment_end_date date());
 
-INSERT INTO jobs (employer_name, job_title, period_time, employment_start_date, employment_end_date) VALUES ('Tek Experts', 'Technical Lead', '2019 - Present', 'random text for now, just  to see', '2019-08-10', '0-0-0');
+INSERT INTO jobs (employer_name, job_title, period_time, employment_start_date, employment_end_date) VALUES ('Tek Experts', 'Technical Lead', '2019 - Present', 'random text for now, just  to see', '2019-08-10', '2023-10-10');
