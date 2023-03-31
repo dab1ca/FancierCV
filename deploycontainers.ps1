87210d6fa98979a288fe0fe1427a880ec2d436ad
dab1ca <45315505+dab1ca@users.noreply.github.com>
2023-03-31 19:34:40 +0300

    Create deploycontainers.ps1

---
+++ deploycontainers.ps1
@@ -0,0 +1,19 @@
+#Connect to Azure
+
+Connect-AzAccount -Identity
+
+#Destroy running app
+
+Remove-AzContainerGroup -Name cv-app -ResourceGroupName ttsvtest
+#Build the new containers and deploy
+$env = New-AzContainerInstanceEnvironmentVariableObject -Name MARIADB_ROOT_PASSWORD -Value Password1
+$port1 = New-AzContainerInstancePortObject -Port 80 -Protocol TCP
+$port2 = New-AzContainerInstancePortObject -Port 3306 -Protocol TCP
+$container1 = New-AzContainerInstanceObject -Name web -Image dab1ca/cv:cv-appaci -RequestCpu 2 -RequestMemoryInGB 2.5 -Port $port1
+$container2 = New-AzContainerInstanceObject -Name db -Image dab1ca/cv:cv-db -RequestCpu 2 -RequestMemoryInGB 2.5 -Port $port2 -EnvironmentVariable @( $env )
+
+New-AzContainerGroup -ResourceGroupName ttsvtest -Name cv-app -Location uksouth -Container @($container1, $container2) -IpAddressType Public
+
+Invoke-AzContainerInstanceCommand -ContainerGroupName cv-app -ContainerName web -ResourceGroupName ttsvtest -Command "sh /usr/local/bin/hosts-entry.sh"
+
+Invoke-AzContainerInstanceCommand -ContainerGroupName cv-app -ContainerName web -ResourceGroupName ttsvtest -Command "rm  /usr/local/bin/hosts-entry.sh"
