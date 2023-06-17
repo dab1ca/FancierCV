b504c09b73a277f2925939a37bfb1ec74061629a
dab1ca <tsvetantsvetanov89@gmail.com>
2023-06-17 12:57:55 +0300

    Pre-final, pics added

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -10,8 +10,11 @@ USE cv;
 
 CREATE TABLE jobs (id int primary key auto_increment, job_title varchar(50), employer_name varchar(50), period_time varchar(50), job_description text(5000));
 
-INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Tek Experts', 'Technical Lead', '2019 - Present', 'random text for now, just  to see');
-INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Tek Experts', 'L3 Engineer', '2018-2019', 'more rndm text');
+INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Tek Experts', 'Technical Lead - September 2021 - Present', 'August 2020 - Present', 'As a Technical Lead my main responsibilities include providing technical and procedural guidance to Support Engineers directly, in daily Triages, or via Swarming Channels; performing daily quality and technical reviews on cases and leading the technical onboarding of new joiners.');
+INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Tek Experts', 'L3 Engineer - February 2021 - September 2021', 'August 2020 - Present', 'As a Senior Engineer in the team, besides working with customers, I was responsible for Queue Management and was tasked with helping in the onboardment of new joiners by providing mentorship after the technical training.');
+INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Tek Experts', 'L2 Engineer - August 2020 - February 2021', 'August 2020 - Present', 'As a Support Engineer, my main responsibilities were to provide technical assistance to customers, while resolving different problems related to the monitoring, and deployment of montioring solutions in their environments');
+INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Telus International', 'Senior Support Representative', 'April 2019 - August 2020', 'As a Senior Support Representative, I was part of a team responsible for handling tier 1 techical escalations related to issues with an online game');
+INSERT INTO jobs (employer_name, job_title, period_time, job_description) VALUES ('Telus International', 'Support Representative April 2019 -  August 2020', 'April 2019 - August 2020', 'As a Support Rep for Telus, my main responsibilities were to provide basic customer support services to the users of the client services.');
 
 CREATE TABLE education (id int primary key auto_increment, specialty_title varchar(50), university_name varchar(50), university_location varchar(50), period_time varchar(50));
 
@@ -21,6 +24,7 @@ INSERT INTO education (specialty_title, university_name, period_time, university
 CREATE TABLE certifications (id int primary key auto_increment, cert_title varchar(50), issuer_name varchar(50), date_of_issue date, certificate_image longblob);
 
 INSERT INTO certifications (cert_title, issuer_name, date_of_issue, certificate_image) VALUES ('Azure Administrator Associate', 'Microsoft', '2023-03-04', LOAD_FILE('/var/lib/mysql/images/az104.png'));
+INSERT INTO certifications (cert_title, issuer_name, date_of_issue, certificate_image) VALUES ('Containerisation CI/CD & Monitoring', 'Software University', '2023-06-14', LOAD_FILE('/var/lib/mysql/images/cm.png'));
 INSERT INTO certifications (cert_title, issuer_name, date_of_issue, certificate_image) VALUES ('Containerisation CI/CD & Monitoring', 'Software University', '2023-03-28', LOAD_FILE('/var/lib/mysql/images/containers.png'));
 INSERT INTO certifications (cert_title, issuer_name, date_of_issue, certificate_image) VALUES ('Linux Server Administration', 'Software University', '2022-10-24', LOAD_FILE('/var/lib/mysql/images/lsa.png'));
 INSERT INTO certifications (cert_title, issuer_name, date_of_issue, certificate_image) VALUES ('Windows Server Administration', 'Software University', '2022-07-25', LOAD_FILE('/var/lib/mysql/images/wsa.png'));
--- web/index.php
+++ web/index.php
@@ -10,6 +10,21 @@
 	<link rel="stylesheet" type="text/css" href="reset-fonts-grids.css" media="all" /> 
 	<link rel="stylesheet" type="text/css" href="fonts.css" media="all" />
 
+	<style>
+		.zoom {
+  			padding: 0px;
+  			background-color: green;
+ 			transition: transform .2s; /* Animation */
+  			width: 50px;
+ 			height: 80px;
+ 			margin: 0 auto;
+		}
+
+		.zoom:hover {
+  			transform: scale(4.0); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+		}
+	</style>
+
 </head>
 <body>
 
@@ -25,6 +40,7 @@
 
 				<div class="yui-u">
 					<div class="contact-info">
+						<img height="120" width="120" src="pic.png">
 						<h3><a id="pdf" href="#" target="_blank" download="PDF">Download PDF</a></h3>
 						<h3><a href="mailto:tsvetantsvetanov89@gmail.com">tsvetantsvetanov89@gmail.com</a></h3>
 						<h3>(359) - 888-593756</h3>
@@ -43,7 +59,7 @@
 						</div>
 						<div class="yui-u">
 							<p class="enlarge">
-								Experienced Cloud Support Engineer, currently working as a Technical Lead in the Azure Monitoring and Automation team at Tek Experts. A naturally curious persion with passion to learn about emerging technologies. 
+								Professional with 5+ years of experience with international businesses within complex and multicultural environments. Passionate and self-driven individual with excellent communication and problem-solving skills. I work efficiently under pressure and have strong attention to detail in order to complete strategic goals. Flexible and able to adapt to any change. 
 							</p>
 						</div>
 					</div><!--// .yui-gf -->
@@ -54,47 +70,52 @@
 						</div>
 						<div class="yui-u">
 
-							<div class="talent">
-								<h2>Problem-Solving</h2>
-								<p> Research and analytical skills</p>
-								<p> Decision making</p>
+							<div class="talent" style="horizontal-align:center">
+								<h2 style="horizontal-align:center">Azure</h2>
+								<p style="horizontal-align:center">Automation & Configuration Management</p>
+								<p style="horizontal-align:center">Monitoring & Alerting</p>
+								<p style="horizontal-align:center">Active Directory</p>
+								<p style="horizontal-align:center">IaC with ARM/Bicep</p>
 							</div>
 
 							<div class="talent">
-								<h2>Communication</h2>
-								<p>Strong interpersonal skills</p>
-								<p>Team player</p>
-								</div>
+								<h2>Server Administration</h2>
+								<p>Windows</p>
+								<p>Linux</p>
+								<p>Docker</p>
+							</div>
 
-								<div class="talent">
-									<h2>Organization</h2>
-									<p>Time management</p>
-									<p>Drive</p>
-								</div>
+							<div class="talent">
+								<h2>Programming</h2>
+								<p>C#</p>
+								<p>HTML</p>
+								<p>PHP</p>
+								<p>Ruby</p>
+							</div>
 						</div>
 					</div><!--// .yui-gf -->
 
 					<div class="yui-gf">
 						<div class="yui-u first">
-							<h2>Technical</h2>
+							<h2>Tools</h2>
 						</div>
 						<div class="yui-u">
 							<ul class="talent">
-								<li>Windows</li>
-								<li>Linux</li>
-								<li class="last">Active Directory</li>
+								<li>Terraform</li>
+								<li>Jenkins</li>
+								<li class="last">Postman</li>
 							</ul>
 
 							<ul class="talent">
-								<li>Azure</li>
-								<li>PowerShell/Bash</li>
-								<li class="last">Terraform</li>
+								<li>Powershell</li>
+								<li>Bash</li>
+								<li class="last">Azure CLI</li>
 							</ul>
 
 							<ul class="talent">
-								<li>C#</li>
-								<li>HTML/CSS</li>
-								<li class="last">MS Office</li>
+								<li>Ansible</li>
+								<li>Chef</li>
+								<li class="last">Puppet</li>
 							</ul>
 						</div>
 					</div><!--// .yui-gf-->
@@ -112,31 +133,72 @@
 					 
 						try {
 						   $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
-						   $jobsquery = $connection->query("SELECT employer_name, job_title, period_time, job_description FROM jobs ORDER BY id ASC");
-						   $jobs = $jobsquery->fetchAll();
+						   $employerquery = $connection->query("SELECT DISTINCT employer_name FROM jobs ORDER BY id ASC");
+						   $employers = $employerquery->fetchAll();
 						   
-						   if (empty($jobs)) {
+						   if (empty($employers)) {
 							  echo "<tr><td>Няма данни.</td></tr>\n";
 						   } else {
-								for ($i = 0; $i < count($jobs); $i++) {
-									$job = $jobs[$i];
-									if ($i == (count($jobs) - 1)) {
+								for ($i = 0; $i < count($employers); $i++) {
+									$employer = $employers[$i];
+									if ($i == (count($employers) - 1)) {
 										print "<div class='job last'>
-												<h2>{$job['employer_name']}<br></br></h2>
-												<h3>{$job['job_title']}</h3>	
-												<h4>{$job['period_time']}</h4>
-												<p>{$job['job_description']}</p>
-												</div>";
+												<h2>{$employer['employer_name']}<br></br></h2>";
+										try {
+											$jobsquery = $connection->query("SELECT employer_name, job_title, period_time, job_description FROM jobs WHERE employer_name = \"{$employer['employer_name']}\" ORDER BY id ASC");
+											$jobs = $jobsquery->fetchAll();
+										
+											if (empty($jobs)) {
+												echo "<tr><td>Няма данни.</td></tr>\n";
+											} else {
+												for ($k = 0; $k < count($jobs); $k++) {
+													$job = $jobs[$k];
+													print "<h3>{$job['job_title']}</h3>	
+														   <h4>{$job['period_time']}</h4>
+														   <p>{$job['job_description']}</p>";
+													if ($k == (count($jobs) - 1)) {
+														print "</div>";
+													}
+												}
+											}
+										}
+										catch (PDOException $e) {
+											print "<tr><td><div align='center'>\n";
+											print "Няма връзка към базата. Опитайте отново. <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: block;';\">Детайли</a><br/>\n";
+											print "<span id='error' style='display: none;'><small><i>".$e->getMessage()." <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: none;';\">Скрий</a></i></small></span>\n";
+											print "</div></td></tr>\n";
+										}	
 									} else {
 										print "<div class='job'>
-												<h2>{$job['employer_name']}<br></br></h2>
-												<h3>{$job['job_title']}</h3>	
-												<h4>{$job['period_time']}</h4>
-												<p>{$job['job_description']}</p>
-												</div>";
+												<h2>{$employer['employer_name']}<br></br></h2>";
+										
+										try {
+											$jobsquery = $connection->query("SELECT employer_name, job_title, period_time, job_description FROM jobs WHERE employer_name = \"{$employer['employer_name']}\" ORDER BY id ASC");
+											$jobs = $jobsquery->fetchAll();
+										
+											if (empty($jobs)) {
+												echo "<tr><td>Няма данни.</td></tr>\n";
+											} else {
+												for ($k = 0; $k < count($jobs); $k++) {
+													$job = $jobs[$k];
+													print "<h3>{$job['job_title']}</h3>	
+													<h4>{$job['period_time']}</h4>
+													<p>{$job['job_description']}</p>";
+													if ($k == (count($jobs) - 1)) {
+														print "</div>";
+													}
+												}
+											}
+										}
+										catch (PDOException $e) {
+											print "<tr><td><div align='center'>\n";
+											print "Няма връзка към базата. Опитайте отново. <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: block;';\">Детайли</a><br/>\n";
+											print "<span id='error' style='display: none;'><small><i>".$e->getMessage()." <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: none;';\">Скрий</a></i></small></span>\n";
+											print "</div></td></tr>\n";
+										}
 									}
-								}
-						   }
+						   		}
+							}
 						}
 						catch (PDOException $e) {
 						   print "<tr><td><div align='center'>\n";
@@ -200,9 +262,9 @@
 							  echo "<tr><td>Няма данни.</td></tr>\n";
 						   } else {
 							  foreach ($certs as $cert) {
-								print "<tr><td><h3>{$cert['cert_title']}</td><td>{$cert['issuer_name']}</td><td>{$cert['date_of_issue']}</td><td>";
-								print '<img height="320" width="250" src="data:image/jpeg;base64,'.base64_encode( $cert['certificate_image'] ).'"/>';
-								print "</td></h3>";
+								print "<tr><td style=\"vertical-align:top\"><h3>{$cert['cert_title']}&nbsp;&nbsp;</td><td>";
+								print '<div class="zoom"><img height="80" width="50" src="data:image/jpeg;base64,'.base64_encode( $cert['certificate_image'] ).'"/></div>';
+								print "</td><td style=\"vertical-align:top\">&nbsp;{$cert['date_of_issue']},</td><td style=\"vertical-align:top\">&nbsp;{$cert['issuer_name']}</td><tr><td>&nbsp;&nbsp;</td></tr></h3>";
 							  }
 						   }
 						}
@@ -210,7 +272,7 @@
 						   print "<tr><td><div align='center'>\n";
 						   print "Няма връзка към базата. Опитайте отново. <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: block;';\">Детайли</a><br/>\n";
 						   print "<span id='error' style='display: none;'><small><i>".$e->getMessage()." <a href=\"#\" onclick=\"document.getElementById('error').style = 'display: none;';\">Скрий</a></i></small></span>\n";
-						   print "</div></td></tr>\n";
+						   print "</div></td></tr>";
 						}
 				?>
 				</table>
@@ -251,3 +313,4 @@
 
 </body>
 </html>
+
